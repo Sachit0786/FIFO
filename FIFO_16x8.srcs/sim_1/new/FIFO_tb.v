a7a6b7283668e8c15ccf7af751d1680a93574344
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2024-09-27 01:37:03 +0530

    Update FIFO_tb.v

--- FIFO_16x8.srcs/sim_1/new/FIFO_tb.v
+++ FIFO_16x8.srcs/sim_1/new/FIFO_tb.v
@@ -1,26 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 26.09.2024 23:56:35
-// Design Name: 
-// Module Name: FIFO_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
-`timescale 1ns / 1ps
 
 module FIFO_tb;
 
