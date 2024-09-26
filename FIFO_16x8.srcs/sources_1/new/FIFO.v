c3a7d6cc31dcc052bdbf801448b115809df6f3c3
Sachit0786 <99992554+Sachit0786@users.noreply.github.com>
2024-09-27 01:37:24 +0530

    Update FIFO.v

--- FIFO_16x8.srcs/sources_1/new/FIFO.v
+++ FIFO_16x8.srcs/sources_1/new/FIFO.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 26.09.2024 23:27:50
-// Design Name: 
-// Module Name: FIFO
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module FIFO (
     input wire clk,
